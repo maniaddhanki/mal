f9228d3c6affc54a3b379b4e6c345774b65e4fed
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2023-05-26 14:15:48 +0530

    Implemented Reader and Printer in step 1

---
+++ impls/manikanta/reader.js
@@ -0,0 +1,54 @@
+class Reader {
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.position = 0;
+  }
+
+  peek() {
+    return this.tokens[this.position];
+  }
+
+  next() {
+    const token = this.peek();
+    this.position++;
+    return token;
+  }
+}
+const tokenize = (str) => {
+  const re = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]*)/g;
+  return [...str.matchAll(re)].map(x => x[1]);
+};
+const read_list = reader => {
+  const ast = [];
+
+  while (reader.peek() !== ")") {
+    ast.push(read_form(reader));
+  }
+
+  reader.next();
+  return ast;
+};
+const read_atom = reader => {
+  const token = reader.next();
+  if (token.match(/^-?[0-9]+$/)) {
+    return parseInt(token);
+  }
+  return token;
+};
+const read_form = reader => {
+  const token = reader.peek();
+  switch (token) {
+    case "(":
+      reader.next();
+      return read_list(reader);
+
+    default:
+      return read_atom(reader);
+  }
+};
+const read_str = (str) => {
+  const tokens = tokenize(str);
+  const reader = new Reader(tokens);
+  return read_form(reader);
+};
+exports.read_str = read_str;
